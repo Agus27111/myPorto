c53e4b2ba5f9fb7e4fbe3b387a8a98d47ee01b50
Agus2711 <Agussetiawan.belajar@gmail.com>
2023-10-18 01:39:27 +0700

    Add existing file

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -21,20 +21,22 @@ if (timeNow < 12) {
 
 function Intro (){
     return (
-        <div className='section text-light'>
-            <div className='grid-child left'>
-                <h1>Hello I'm Web Developer</h1>
-                <p className="heading" style={customColor}>
+        <div className='container'>
+          <div className='row intro'>      
+            <div className='col intro-left'>
+                <h1 className="heading" style={customColor}>
                     {greething}
-                </p>
+                </h1>
+                <h2>I'm Web Developer</h2>
                 <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 </p>
             </div>
-            <div className='grid-child right'>
-                <img
-                    src=''
+            <div className='col intro-right'>
+                <img className='img-avatar'
+                    src='https://thumbs.dreamstime.com/b/software-language-programmer-avatar-software-language-programmer-avatar-vector-illustration-design-110589781.jpg'
                 />
+            </div>
             </div>
         </div>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
+const mystyle = {
+    color: "white",
+    fontSize: "20px",
+    fontFamily: "Arial"
+  };
+
 function Navbar (){
     return (
         <div>
-                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <nav class="navbar navbar-expand-xxl justify-content-center " style={mystyle}>
         <div class="container">
             <a class="navbar-brand" href="#">Agus Setiawan</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
                 <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">About Me</a>
                 </li>
@@ -18,9 +24,9 @@ function Navbar (){
                 <li class="nav-item">
                 <a class="nav-link" href="#">Recent Work</a>
                 </li>
-                
             </ul>
             </div>
+          
         </div>
         </nav>
         </div>
